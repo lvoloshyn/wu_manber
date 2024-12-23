78b5340a83c3b8eaa8f0a09f078d56200cb5f5f5
Leonid Voloshyn <4700700+lvoloshyn@users.noreply.github.com>
2024-12-23 17:27:34 +0200

    Add type annotations

--- wu_manber.py
+++ wu_manber.py
@@ -1,46 +1,113 @@
-# -*- coding: utf-8 -*-
+from typing import Dict, Generic, List, Tuple, TypeVar, Union
 
-from collections import defaultdict
-from typing import List, Tuple, Union, Sequence
+T = TypeVar("T", str, bytes, List, Tuple)
 
 
-class WuManber:
-    def __init__(self, patterns: List[Union[str, Tuple[str]]]) -> None:
-        self.patterns = patterns
-        self.pattern_to_id = {pattern: i for i, pattern in enumerate(self.patterns)}
+class WuManberSearch(Generic[T]):
+    def __init__(self, patterns: List[T]) -> None:
+        self.patterns: List[T] = patterns
+        self.pattern_to_id: Dict[T, int] = {
+            pattern: i for i, pattern in enumerate(self.patterns)
+        }
 
-        self.m = self.get_min_pattern_length()
+        self.m = self.__get_min_pattern_length()
         self.B = min(2, self.m)  # to support 1-element patterns
 
-        self.shift_table = self.generate_shift_table()
-        self.prefix_table = defaultdict(list)
+        self.shift_table: Dict[Union[T, None], int] = self.__generate_shift_table()
+        self.prefix_table: Dict[T, List[T]] = self.__generate_prefix_table()
+
+    def __get_min_pattern_length(self) -> int:
+        return len(min(self.patterns, key=len))  # type: ignore
+
+    def __generate_prefix_table(self) -> Dict[T, List[T]]:
+        prefix_table: Dict[T, List[T]] = {}
 
         for pattern in self.patterns:
-            prefix = pattern[:self.B]  # using only first m!
-            self.prefix_table[prefix].append(pattern)
+            prefix = pattern[: self.B]  # using only first m!
+            if prefix not in prefix_table:
+                prefix_table[prefix] = []
 
-    def search(self, sequence: Sequence,
-               only_longest: bool = False,
-               return_pattern_id: bool = False) -> List[Tuple[Union[int, str], int, int]]:
-        result = []
+            prefix_table[prefix].append(pattern)
+
+        return prefix_table
+
+    def __generate_shift_table(self) -> Dict[Union[T, None], int]:
+        keys = set()
+
+        shift: Dict[Union[T, None], int] = (
+            {None: self.m - self.B + 1} if self.B > 1 else {None: 0}
+        )
+
+        for pattern in self.patterns:
+            for i in range(self.m - 1):
+                part = pattern[i : i + self.B]
+
+                if len(part) == self.B:
+                    keys.add(part)
+
+        for key in keys:
+            shift[key] = self.m - self.B + 1
+
+        for key in keys:
+            for pattern in self.patterns:
+                if key in pattern[: self.m]:
+                    j = pattern[: self.m].index(key)
+                    shift[key] = min(shift[key], self.m - j - self.B)
+
+        return shift
+
+    @staticmethod
+    def get_longest_spans(
+        spans: List[Tuple[Union[T, int], int, int]]
+    ) -> List[Tuple[Union[T, int], int, int]]:
+        # we assume spans are sorted
+        span_to_pattern: Dict[Tuple[int, int], Union[T, int]] = {
+            (span[1], span[2]): span[0] for span in spans
+        }
+
+        non_overlapping_spans: List[Tuple[int, int]] = []
+
+        for labeled_span in spans:
+            span: Tuple[int, int] = labeled_span[1:]
+            if not non_overlapping_spans:
+                non_overlapping_spans.append(span)
+
+            else:
+                if span[0] >= non_overlapping_spans[-1][1]:
+                    non_overlapping_spans.append(span)
+
+                else:
+                    non_overlapping_spans[-1] = (
+                        non_overlapping_spans[-1][0],
+                        max(span[1], non_overlapping_spans[-1][1]),
+                    )
+
+        return [
+            (span_to_pattern[span], span[0], span[1]) for span in non_overlapping_spans
+        ]
+
+    def search(
+        self, sequence: T, only_longest: bool = False, return_pattern_id: bool = False
+    ) -> List[Tuple[Union[T, int], int, int]]:
+        result: List[Tuple[Union[T, int], int, int]] = []
 
         ix = self.m - 1
         length = len(sequence)
 
         while ix < length:
-            hash_1 = sequence[ix - self.B + 1: ix + 1]
+            hash_1 = sequence[ix - self.B + 1 : ix + 1]
 
-            shift = self.shift_table.get(hash_1, self.shift_table.get("*"))
+            shift = self.shift_table.get(hash_1, self.shift_table[None])
             if shift == 0:
                 # check for matching prefixes
-                hash_2 = sequence[ix - self.m + 1:ix - self.m + 1 + self.B]
+                hash_2 = sequence[ix - self.m + 1 : ix - self.m + 1 + self.B]
 
                 possible_patterns = self.prefix_table.get(hash_2)
 
                 if possible_patterns:
                     for pattern in possible_patterns:
-                        target = sequence[ix - self.m + self.B + 1:]
-                        candidate = pattern[self.B:]
+                        target = sequence[ix - self.m + self.B + 1 :]
+                        candidate = pattern[self.B :]
 
                         if len(target) < len(candidate):
                             continue
@@ -55,7 +122,13 @@ class WuManber:
                             pattern_start = ix - self.m + 1
                             pattern_end = pattern_start + len(pattern)
                             if return_pattern_id:
-                                result.append((self.pattern_to_id[pattern], pattern_start, pattern_end))
+                                result.append(
+                                    (
+                                        self.pattern_to_id[pattern],
+                                        pattern_start,
+                                        pattern_end,
+                                    )
+                                )
 
                             else:
                                 result.append((pattern, pattern_start, pattern_end))
@@ -68,50 +141,3 @@ class WuManber:
             return self.get_longest_spans(result)
 
         return result
-
-    def get_min_pattern_length(self):
-        return len(min(self.patterns, key=len))
-
-    def generate_shift_table(self):
-        keys = set()
-
-        shift = {"*": self.m - self.B + 1} if self.B > 1 else {"*": 0}
-
-        for pattern in self.patterns:
-            for i in range(self.m - 1):
-                part = pattern[i:i + self.B]
-
-                if len(part) == self.B:
-                    keys.add(part)
-
-        for key in keys:
-            shift[key] = self.m - self.B + 1
-
-        for key in keys:
-            for pattern in self.patterns:
-                if key in pattern[:self.m]:
-                    j = pattern[:self.m].index(key)
-                    shift[key] = min(shift.get(key), self.m - j - self.B)
-
-        return shift
-
-    def get_longest_spans(self, spans: List[Tuple[Union[int, str], int, int]]):
-        # we assume spans are sorted
-        span_to_pattern = {(span[1], span[2]): span[0] for span in spans}
-
-        non_overlapping_spans = []
-        for span in spans:
-            span = span[1:]
-            if not non_overlapping_spans:
-                non_overlapping_spans.append(span)
-
-            else:
-                if span[0] >= non_overlapping_spans[-1][1]:
-                    non_overlapping_spans.append(span)
-
-                else:
-                    non_overlapping_spans[-1] = (
-                        non_overlapping_spans[-1][0], max(span[1], non_overlapping_spans[-1][1])
-                    )
-
-        return [(span_to_pattern[span], span[0], span[1]) for span in non_overlapping_spans]
